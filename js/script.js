d24e863fe8b61a108a559ba57fb853ed5a1fcef1
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-18 15:37:00 +0100

    add object

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  // 1 creo oggetto con lista di cose da comprare
+  var addList = {
+
+    "Comprare il pane",
+
+    "Comprare frutta",
+
+    "Comprare verdura"
+
+  }
+
+  // 2 creo un clone del template
+  
+  var template = $(".template ul li").clone();
+
+
+
+
+
+
+
+
+  });
